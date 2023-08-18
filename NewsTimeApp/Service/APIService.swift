656f1d59d17078a645508aeeb812bfea1b532c7e
saifGithube <saifjuini1444@gmail.com>
2023-08-18 16:35:41 +0200

    Merged remote changes

--- NewsTimeApp/Service/APIService.swift
+++ NewsTimeApp/Service/APIService.swift
@@ -9,36 +9,6 @@ import Foundation
 
 // MARK: - this call is to retrieve the  data
 
-//
-//public final class APIService {
-//
-//    private let apiKey = "fb52d3d0c501459eabbb1418a7f3c4bb"
-//    private var sourcesURL: URL {
-//        var components = URLComponents(string: "https://newsapi.org/v2/everything")!
-//        components.queryItems = [
-//            URLQueryItem(name: "q", value: "tesla"),
-//            URLQueryItem(name: "sortBy", value: "publishedAt"),
-//            URLQueryItem(name: "apiKey", value: apiKey)
-//        ]
-//        return components.url!
-//    }
-//
-//    func apiToGetNewsData(completion: @escaping (NewsModel) -> ()) {
-//        URLSession.shared.dataTask(with: sourcesURL) { data, _, error in
-//            if let data = data {
-//                do {
-//                    let empData = try JSONDecoder().decode(NewsModel.self, from: data)
-//                    completion(empData)
-//                } catch {
-//                    print("Error decoding JSON: \(error)")
-//                }
-//            } else if let error = error {
-//                print("Error fetching data: \(error)")
-//            }
-//        }.resume()
-//    }
-//}
-
 public final class APIService {
 
     private let apiKey = "fb52d3d0c501459eabbb1418a7f3c4bb"
